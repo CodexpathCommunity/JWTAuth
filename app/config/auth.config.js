ac3332bfb8115476d47007de96a299a916856a88
CodexpathCommunity <codexpath3@gmail.com>
2021-08-06 21:42:54 +0100

    added the auth config file

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: "codexpath-secret-key",
+};
--- server.js
+++ server.js
@@ -16,6 +16,41 @@ app.use(bodyParser.json());
 //parse request of content type application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${db.Config.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("successfully connected to MongoDb");
+    initial();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user",
+      }).save((err) => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added moderator to roles collection");
+      });
+      new Role({
+        name: "admin",
+      }).save((err) => {
+        if (err) {
+          console.log("added admin to roles collection");
+        }
+      });
+    }
+  });
+}
+
 //simple route
 app.get("/", (req, res) => {
   res.json({ message: "welcome to codexpath application" });
