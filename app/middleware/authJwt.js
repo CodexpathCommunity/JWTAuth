cf766e360e149f602805de559159ac7874703a2a
CodexpathCommunity <codexpath3@gmail.com>
2021-08-06 23:01:46 +0100

    added the authjwt functions

--- app/middleware/authJwt.js
+++ app/middleware/authJwt.js
@@ -25,7 +25,7 @@ isAdmin = (req, res, next) => {
       res.status(500).send({ message: err });
       return;
     }
-    Role.findById(
+    Role.find(
       {
         _id: { $in: user.roles },
       },
@@ -34,7 +34,53 @@ isAdmin = (req, res, next) => {
           res.status(500).send({ message: err });
           return;
         }
+
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "admin") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Require admin role!" });
+        return;
       }
     );
   });
 };
+
+isModerator = (req, res, next) => {
+  User.findById(req.userId).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    Role.find(
+      {
+        _id: { $in: user.roles },
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+        }
+        res.status(403).send({ message: "Require Moderator Role!" });
+        return;
+      }
+    );
+  });
+};
+
+const authJwt = {
+  verifyToken,
+  isAdmin,
+  isModerator,
+};
+
+module.exports = authJwt;
