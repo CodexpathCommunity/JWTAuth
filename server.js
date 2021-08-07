ca854fbec9a12fb39daf3b867ab8918ffa3c603a
CodexpathCommunity <codexpath3@gmail.com>
2021-08-07 13:42:21 +0100

    added the server functions

--- server.js
+++ server.js
@@ -63,7 +63,10 @@ function initial() {
 app.get("/", (req, res) => {
   res.json({ message: "welcome to codexpath application" });
 });
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
+//set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
